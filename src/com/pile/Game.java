ecd9eccef12bc4c090b5022adaed46f6e2a9e2e0
doomdragon0 <alextu85@yahoo.ca>
2018-05-25 09:03:01 -0400

    Merge branch 'master' of C:\Users\alext\IdeaProjects\Pile with conflicts.

--- src/com/pile/Game.java
+++ src/com/pile/Game.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 
 public class Game extends Canvas {
 	public static final int WIDTH = 1000;
-	public static final int HEIGHT = 600;
+	public static final int HEIGHT = 800;
 	private JFrame frame;
 	private GameLogic gameLogic;
 	private boolean running = true;
@@ -20,7 +20,7 @@ public class Game extends Canvas {
 			Resources.load();
 		} catch (Exception e) { e.printStackTrace(); }
 		gameLogic = new GameLogic();
-		Dimension d = new Dimension(Game.WIDTH, Game.HEIGHT-1);
+		Dimension d = new Dimension(Game.WIDTH, Game.HEIGHT);
 		setPreferredSize(d);
 
 		setFocusable(true);		// These two lines allow
@@ -32,8 +32,11 @@ public class Game extends Canvas {
 
 		frame.add(this, BorderLayout.CENTER);
 		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.setUndecorated(true);
+		System.out.println(frame.getContentPane().getHeight());
+		System.out.println(frame.getContentPane().getWidth());
 		run();
 	}
 
