58e95510dd5a13e25c73ecff409cef862bf901c6
doomdragon0 <alextu85@yahoo.ca>
2018-06-10 19:19:12 -0400

    pretty.

--- src/com/pile/state/MenuState.java
+++ src/com/pile/state/MenuState.java
@@ -5,11 +5,16 @@ import com.pile.Input;
 import com.pile.ui.Button;
 import com.pile.ui.ButtonManager;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MenuState implements GameState {
 	private ButtonManager buttons;
+	BufferedImage background, piles;
 
 	public MenuState() {
 		this.buttons = new ButtonManager();
@@ -17,6 +22,11 @@ public class MenuState implements GameState {
 		Button controlButton = new Button("Controls", Game.WIDTH/2, Game.HEIGHT/2 + 200, 70f, new Color(0,0,0), new Color(0,0,0), new Color(0,0,0));
 		buttons.add(playButton);
 		buttons.add(controlButton);
+		try{
+			background = ImageIO.read(new File("assets/images/REAL/MenuBG.png"));
+			piles = ImageIO.read(new File("assets/images/REAL/PilesFontLogo.png"));
+		}catch (IOException e){
+		}
 	}
 
 	@Override
@@ -26,6 +36,8 @@ public class MenuState implements GameState {
 
 	@Override
 	public void render(Graphics g) {
+		g.drawImage(background,Game.WIDTH/2 - background.getWidth()/2, Game.HEIGHT/2 - background.getHeight()/2,null);
+		g.drawImage(piles,Game.WIDTH/2 - piles.getWidth()/2,25,null);
 		buttons.render(g);
 	}
 }
